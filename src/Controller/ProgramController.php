2e78da64e3f58aca470b5016c4886ecc99ec637a
Océane Raubitschek <oceane>
2022-01-03 20:04:49 +0100

    challenge 3, advanced routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/program", name="program_")
+*/
+
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Donkey Séries',
          ]);
+    }
+
+    /**
+    * @Route("/{id<\d+>}",name="show", methods={"GET"})
+    */
+    public function show(): Response
+    {
+        return $this->render('program/show.html.twig', ['id'=> 4]);
     }
 }
